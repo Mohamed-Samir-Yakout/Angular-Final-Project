8db2723ac18ba61d6ede71f2c119ea7ad847e0f6
ahmedmagdyorabi <ahmed.magdy.orabi1993@gmail.com>
2021-02-01 04:22:31 +0200

    ahmed magdy 01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { AccountSettingsComponent } from './Components/account-settings/account-settings.component';
+import { AdminDashboardComponent } from './Components/admin-dashboard/admin-dashboard.component';
+import { MyOrderComponent } from './Components/my-order/my-order.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { PrivacyComponent } from './Components/privacy/privacy.component';
+import { TermsComponent } from './Components/terms/terms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Admin', component: AdminDashboardComponent},
+  {path: 'Privacy', component: PrivacyComponent},
+  {path: 'TermsAndConditions', component: TermsComponent},
+  {path: 'About-Us', component: AboutUsComponent},
+  {path: 'Home', component: AdminDashboardComponent},
+  {path: 'Delivery', component: AdminDashboardComponent},
+  {path: 'Dine-out', component: AdminDashboardComponent},
+  {path: 'Gift-Meals', component: AdminDashboardComponent},
+  {path: 'My-Orders', component: AdminDashboardComponent},
+  {path: 'Account-Settings', component: AccountSettingsComponent},
+  {path: 'My-Order', component: MyOrderComponent},
+  {path: '**', component: NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { TermsComponent } from './Components/terms/terms.component';
+import { PrivacyComponent } from './Components/privacy/privacy.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { AdminDashboardComponent } from './Components/admin-dashboard/admin-dashboard.component';
+import { AccountSettingsComponent } from './Components/account-settings/account-settings.component';
+import { MyOrderComponent } from './Components/my-order/my-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    TermsComponent,
+    PrivacyComponent,
+    NotFoundComponent,
+    AdminDashboardComponent,
+    AccountSettingsComponent,
+    MyOrderComponent
   ],
   imports: [
     BrowserModule,
