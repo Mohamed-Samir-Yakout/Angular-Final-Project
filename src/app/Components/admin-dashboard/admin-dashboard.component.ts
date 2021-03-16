169c7ce476b5f8d285b26f3bb2dc6b285d3432ea
mohamed samir <m.samir.ahmed24@gmail.com>
2021-03-16 12:40:12 +0200

    hosting step

--- src/app/Components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/Components/admin-dashboard/admin-dashboard.component.ts
@@ -10,7 +10,7 @@ export class AdminDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.location.href = 'http://localhost:4500'
+    window.location.href = 'https://admin-panel-host.herokuapp.com'
   }
 
 }
